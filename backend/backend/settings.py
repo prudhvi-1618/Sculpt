c7cce6ef7e3f8285df2cefbefd2df54578446f5a
prudhvi-1618 <nirujogiprudhvi@gmail.com>
2025-04-16 16:20:12 +0530

    unbuild static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_ROOT = None 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
